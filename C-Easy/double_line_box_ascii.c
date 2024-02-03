f8138e948f706ad3b0e973af58283917ecbe610f
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-02-03 13:20:28 +0200

    Add files via upload

---
+++ C-Easy/double_line_box_ascii.c
@@ -0,0 +1,15 @@
+//Trikidou Eleonora
+/*A simple program prining double line box using ASCII table characters
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%c%c%c%c%c%c%c\n",201,205,205,205,205,205,187);
+    printf("%c  C  %c",186,186);
+    putchar('\n');
+    printf("%c%c%c%c%c%c%c\n",200,205,205,205,205,205,188);
+    return 0;
+}
