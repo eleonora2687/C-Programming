b4d139b1637d9fa74b37ab79fd6c95a3d83a5683
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-02-04 13:53:41 +0200

    Add files via upload

---
+++ C-Medium/Tree.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    int height, i, j, k;
+    scanf("%d",&height);
+
+    if(height>=4 && height<=20)
+    {
+       for(i=1;i<=height-1;i++)
+    {
+       for(j=height-1-i;j>=1;j--)
+          putchar(' ');
+
+       for(k=1;k<=2*i-1;k++)
+          printf("*");
+
+       putchar('\n');
+    }
+
+    i=1;
+
+       for(j=height-1-i;j>=1;j--)
+          putchar(' ');
+
+    printf("*");
+    putchar('\n');
+
+    }
+
+    return 0;
+}
